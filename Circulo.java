0c77e02075cd435982f2971257710e6db802f2c4
Gabriel Lemes de França Ribeiro <gabriel.lribeiro@a.ucb.br>
2024-11-08 22:34:50 -0300

    Create Circulo.java

---
+++ Circulo.java
@@ -0,0 +1,14 @@
+package formas;
+
+public class Circulo implements AreaCalculavel {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * raio * raio;
+    }
+}
